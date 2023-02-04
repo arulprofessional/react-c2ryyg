a860fecea9f5f55bdb730297ea121d7ff755b9d3
arulprofessional <106566414+arulprofessional@users.noreply.github.com>
2023-02-04 16:16:31 +0530

    Test first Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>My Todo List</h1>
+      <div>
+        <h2>Title</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
